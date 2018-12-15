15e5c26d19dcf6b995d8b57bc26c52521232a7b2
renatolaranja <matthew.b.geiger@gmail.com>
2018-12-14 19:28:15 -0500

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+//create global variables
+
+
+//create a function for the countdown timer for 1 minute
+
+
+//click start button to start timer and display questions
+
+//create function for questions
+
+
+//display score of answered questions once time is up
